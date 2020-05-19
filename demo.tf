0aa1a79a75e2ca20426dea8a6784f9f7fece76d2
wangwenbo <47601801+wangwenbo0107@users.noreply.github.com>
2020-05-19 17:25:11 +0900

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,13 +1,28 @@
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+provider "aws" {
+  version = "2.33.0"
+
+  region = var.aws_region
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
+provider "random" {
+  version = "2.2"
+}
+
+resource "random_pet" "table_name" {}
+
+resource "aws_dynamodb_table" "tfc_example_table" {
+  name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+  read_capacity  = var.db_read_capacity
+  write_capacity = var.db_write_capacity
+  hash_key       = "UUID"
+
+  attribute {
+    name = "UUID"
+    type = "S"
+  }
+
+  tags = {
+    user_name = var.tag_user_name
   }
 }
